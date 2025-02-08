3ebf8372ef277e543cd5bda109a9495828a30677
justine <justine@DESKTOP-SGN56DU>
2025-02-08 14:09:13 +0800

    Add project files.

---
+++ MVC/Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
